0ea9b3ac683e51b0321988c8f00403f9f39e7724
traceback <q1w2e3r4>
2019-06-05 19:57:39 +0430

    login backend

--- database/migrations/2019_06_03_175232_laratrust_setup_tables.php
+++ database/migrations/2019_06_03_175232_laratrust_setup_tables.php
@@ -25,6 +25,7 @@ class LaratrustSetupTables extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
+            $table->string('category')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
         });
