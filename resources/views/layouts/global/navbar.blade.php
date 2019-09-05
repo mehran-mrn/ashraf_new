6a3aa0db82de1bbafcf600efd7d7e25adf0a5781
traceback <mehran1990ss@yahoo.com>
2019-09-05 20:54:58 +0430

    adv links

--- resources/views/layouts/global/navbar.blade.php
+++ resources/views/layouts/global/navbar.blade.php
@@ -4,20 +4,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <a class=" pull-left flip m-1" href="{{route('home')}}"><img
-                                src="{{ URL::asset('/public/assets/global/images/logo-wide@2x.png') }}" alt=""></a>
-                </div>
-                <div class="col-md-3">
                     <nav>
                         <ul class="list-inline sm-text-center text-left flip mt-0">
-
                             <li> <a class="text-white" href="{{route('home')}}">{{__('messages.FAQ')}}</a> </li>
                             <li class="text-white">|</li>
                             <li> <a class="text-white" href="{{route('home')}}">{{trans('messages.support')}}</a> </li>
                         </ul>
                     </nav>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                     <div class="widget m-0 mt-5 no-border">
                         <ul class="list-inline text-right sm-text-center">
                             <li class="pl-10 pr-10 mb-0 pb-0">
@@ -46,7 +41,6 @@
         <div class="header-nav-wrapper  bg-lighter">
             <div class="container">
                 <nav id="menuzord-right" class="menuzord default no-bg"> <a class="menuzord-brand pull-left flip" href="index-mp-layout1.html"><img src="images/logo-wide.png" alt=""></a>
-
                     <ul class="list-inline pull-right flip hidden-sm hidden-xs ">
                         @if (Auth::check())
                             <li>
@@ -73,6 +67,8 @@
                             </li>
                         @endif
                     </ul>
+                    <a class="menuzord-brand pull-left flip" href="{{route('home')}}"><img
+                                src="{{ URL::asset('/public/assets/global/images/logo-wide@2x.png') }}" alt=""></a>
                     <ul class="menuzord-menu pull-left flip ">
                         <li><a href="{{route('index')}}">{{trans('messages.home')}}</a></li>
 
@@ -103,8 +99,7 @@
                         @if(session()->get('cart'))
                             <li><a href="{{route('store_order')}}">{{__('messages.buy_basket')}}</a></li>
                         @endif
-                    </ul>
-                </nav>
+                    </ul>                </nav>
             </div>
         </div>
     </div>
