e651dcf8680054055b4edb945876d20949ce78c4
Milad Kardgar <mk.kardgar@gmail.com>
2019-09-19 16:35:49 +0430

    add gateway module

---
+++ app/Http/Controllers/CharityChampionsCommentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\charity_champions_comment;
+use Illuminate\Http\Request;
+
+class CharityChampionsCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\charity_champions_comment  $charity_champions_comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(charity_champions_comment $charity_champions_comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\charity_champions_comment  $charity_champions_comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(charity_champions_comment $charity_champions_comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\charity_champions_comment  $charity_champions_comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, charity_champions_comment $charity_champions_comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\charity_champions_comment  $charity_champions_comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(charity_champions_comment $charity_champions_comment)
+    {
+        //
+    }
+}
